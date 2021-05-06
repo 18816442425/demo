5242de2e75eb11367c367d2978f7d79632b99753
温闽华 <wenminhua@meizu.com>
2021-05-06 13:39:21 +0800

    Update TUaApplicationController.java

--- src/main/java/com/example/demo/back/controller/TUaApplicationController.java
+++ src/main/java/com/example/demo/back/controller/TUaApplicationController.java
@@ -29,7 +29,7 @@ public class TUaApplicationController  {
     ITUaApplicationService itUaApplicationService;
 
     @GetMapping("/appList")
-    @ApiOperation(value = "获取应用信息列表2", notes = "分页获取应用信息")
+    @ApiOperation(value = "获取应用信息列表5", notes = "分页获取应用信息")
     public ResponseData getEmployLists(@RequestParam(value = "pageNo",defaultValue = "1")
                                            @ApiParam(value = "当前页码",required = true) Integer pageNo,
                                        @RequestParam(value = "size",defaultValue = "5")
